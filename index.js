fe82437d54e6146cbf5ff16a97fe8c49c090aebb
Aaron Na <syn228@nyu.edu>
2018-11-21 19:46:07 -0800

    edited root index to adjust for other files

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const router = require('./router')
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-});
+mongoose.connect('mongodb://localhost:27017/myapp')
+
+
+
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*'}))
+router(app)
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app)
+server.listen(port);
+console.log("Server listening on:", port)
